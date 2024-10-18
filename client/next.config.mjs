8d043ea1709ad708d14450c8bd7ee341545f19b6
ryoshi1001 <ryoshi16421@proton.me>
2024-10-18 02:47:37 -0700

    domain name update

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,7 +2,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-   domains: ["me_whats_app.vercel.app", "localhost"],
+   domains: ["me-whats-app.vercel.app", "localhost"],
   }
  };
  
